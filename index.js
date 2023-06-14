d6924b94ab688a59ae172ec04a681bad858d8947
joshua <josh.newbill08@hotmail.com>
2023-06-13 21:09:50 -0500

    Method complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+class Cat {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+
+    }
+     //... Method
+    speak(){
+        //console.log(`${this.name} says meow!`)
+        return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+
+    }
+    //... Method
+    speak(){
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+
+    }
+    //... Method
+    speak(){
+        if(this.sex === "male"){
+            return `It's me! ${this.name}, the parrot!`
+        } else{
+            return `${this.name} says squawk!`
+        }
+        
+    }
+  }
